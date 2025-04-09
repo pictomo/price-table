81fe746fadbbe9d31ee99359c617974c5f163ba7
pictomo <tomo.pict@gmail.com>
2025-04-09 15:08:34 +0900

    [update]データリクエスト&挿入機能まで

--- price-table.js
+++ price-table.js
@@ -2,65 +2,92 @@ const fs = require("fs");
 const path = require("path");
 const { parse } = require("csv-parse/sync");
 
-const now = new Date();
-
-// ファイルパスの定義
-const filePath = path.join(__dirname, "price-table.csv");
-
-// 二次元配列をCSV形式の文字列に変換する関数
-list2csv = (list) => {
-  return list.map((row) => row.join(",")).join("\n") + "\n";
-};
-
-// price-table.csv がなければ新規作成
-if (!fs.existsSync(filePath)) {
+(async () => {
   // デフォルトの通貨
   const defaultCurrencies = ["bitcoin", "ethereum", "litecoin", "dogecoin"];
+  // ベース通貨
+  const baseCurrency = "jpy";
+  // coingecko API のレートリミット ~30calls/minute
+  // https://docs.coingecko.com/reference/common-errors-rate-limit#rate-limit
+  const requestSpeed = 10000;
 
-  // 初期配列の定義
-  const initialData = [["date", ...defaultCurrencies]];
+  const now = new Date();
 
-  // 初期データをファイルに保存
-  const csvString = list2csv(initialData);
-  fs.writeFileSync(filePath, csvString, "utf-8");
-  console.log("price-table.csv を新規作成しました。");
-}
+  // ファイルパスの定義
+  const filePath = path.join(__dirname, "price-table.csv");
 
-// CSVファイルを読み込む
-const csvData = fs.readFileSync(filePath, "utf-8");
+  // 二次元配列をCSV形式の文字列に変換する関数
+  list2csv = (list) => {
+    return list.map((row) => row.join(",")).join("\n") + "\n";
+  };
 
-// パースして2次元配列として出力
-const data = parse(csvData, {
-  columns: false,
-  skip_empty_lines: true,
-  cast: (value) => {
-    return value === "" ? null : value;
-  },
-});
+  // price-table.csv がなければ新規作成
+  if (!fs.existsSync(filePath)) {
+    // 初期配列の定義
+    const initialData = [["date", ...defaultCurrencies]];
 
-const currencyCount = data[0].length - 1;
+    // 初期データをファイルに保存
+    const csvString = list2csv(initialData);
+    fs.writeFileSync(filePath, csvString, "utf-8");
+    console.log("price-table.csv を新規作成しました。");
+  }
 
-// 行が一つもなければ、本日の行を追加
-if (data.length === 1) {
-  const newRow = [
-    now.toLocaleDateString("sv-SE"),
-    ...Array(currencyCount).fill(null),
-  ];
-  data.push(newRow);
-}
+  // CSVファイルを読み込む
+  const csvData = fs.readFileSync(filePath, "utf-8");
 
-// 本日までの行を追加
-let lastDate = new Date(data[data.length - 1][0]);
-lastDate.setDate(lastDate.getDate() + 1);
-while (lastDate < now) {
-  const newRow = [
-    lastDate.toLocaleDateString("sv-SE"),
-    ...Array(currencyCount).fill(null),
-  ];
-  data.push(newRow);
+  // パースして2次元配列として出力
+  const data = parse(csvData, {
+    columns: false,
+    skip_empty_lines: true,
+    cast: (value) => {
+      return value === "" ? null : value;
+    },
+  });
+
+  const currencyCount = data[0].length - 1;
+
+  // 行が一つもなければ、本日の行を追加
+  if (data.length === 1) {
+    const newRow = [
+      now.toLocaleDateString("sv-SE"),
+      ...Array(currencyCount).fill(null),
+    ];
+    data.push(newRow);
+  }
+
+  // 本日までの行を追加
+  let lastDate = new Date(data[data.length - 1][0]);
   lastDate.setDate(lastDate.getDate() + 1);
-}
+  while (lastDate < now) {
+    const newRow = [
+      lastDate.toLocaleDateString("sv-SE"),
+      ...Array(currencyCount).fill(null),
+    ];
+    data.push(newRow);
+    lastDate.setDate(lastDate.getDate() + 1);
+  }
 
-// ファイルに保存
-const csvString = list2csv(data);
-fs.writeFileSync(filePath, csvString, "utf-8");
+  // 価格データを挿入
+  for (let i = 1; i < data.length; i++) {
+    date = new Date(data[i][0]);
+    for (let currencyNum = 1; currencyNum <= currencyCount; currencyNum++) {
+      // データがない場合は null を挿入
+      if (data[i][currencyNum] === null) {
+        await new Promise((resolve) => setTimeout(resolve, requestSpeed));
+        response = await fetch(
+          `https://api.coingecko.com/api/v3/coins/${
+            data[0][currencyNum]
+          }/history?date=${date.toLocaleDateString("brx-IN")}`
+        );
+        const json = await response.json();
+        console.log(json);
+        price = json.market_data.current_price[baseCurrency];
+        data[i][currencyNum] = price;
+      }
+    }
+  }
+
+  // ファイルに保存
+  const csvString = list2csv(data);
+  fs.writeFileSync(filePath, csvString, "utf-8");
+})();
